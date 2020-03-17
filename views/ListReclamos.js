f579186d25dc64cbba703f5a71004bfa99933235
Hernan Casé <hcase@ecolan.com>
2020-03-17 11:31:41 -0300

    Fixed Height of ReclamosView

--- views/ListReclamos.js
+++ views/ListReclamos.js
@@ -6,7 +6,7 @@ import { connect } from 'react-redux';
 import { setReclamo } from '../redux/actions/index';
 
 import Card from '../components/CardReclamos';
-import { StyleSheet, View, TouchableOpacity,TouchableWithoutFeedback } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, Text, SafeAreaView } from 'react-native';
 import theme from '../styles';
 
 
@@ -33,47 +33,6 @@ const ListReclamos = (props) => {
 
     }
 
-    
-    
-
-        /*function fillTableRow(){
-            if (reclamos){
-                return (
-                    reclamos.map((reclamo, i) => {
-                        return (
-                        <TouchableOpacity key={i}  onPress={(e) =>{ setSections(reclamo)}}>
-                            <Card  key={i} >
-                            <View style={styles.card__title}>
-                                <View>
-                                    <Text style={{ fontSize: 18, color: 'black' }}> {"# "+reclamo.numero}</Text> 
-                                    <Text style={styles.card_body_title} >Inconveniente:</Text>
-                                    <Text style={styles.card_body_desc} >{reclamo.inconveniente}</Text>
-                                </View>
-                                <View>
-                                    <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                                        <Icon name="phone" color='#011627'/>
-                                    <Text style={styles.card_title_numero}> {reclamo.telefono}</Text> 
-                                </View>
-                                    <Text style={styles.card_title_fecha}>{reclamo.fecha}</Text>
-                                </View>
-                            </View>
-                            <Text style={styles.card_body_title}>Detalle:</Text>
-                            {
-                               isCollapsed ? 
-
-                               
-                                    <Text key={index} style={styles.card_body_desc}>{reclamo.detalle}</Text>
-                               :
-                               <Text numberOfLines={maxString} style={styles.card_body_desc}>{reclamo.detalle}</Text>
-                            }
-                            </Card>
-                        </TouchableOpacity>
-                        );
-                    })
-                )
-            } 
-        };*/
-
         function fillTableRow(){
             if (reclamos){
                 return (
@@ -94,35 +53,12 @@ const ListReclamos = (props) => {
         };
 
         return (
-            <View style={{ margin: 10 }}>
+            <View style={{ padding: 10, paddingBottom: 70 }}>
                 {fillTableRow()}
             </View>
         );
 }
 
-/*
-                <Accordion
-                activeSections={section}
-                //for any default active section
-                sections={reclamos}
-                //title and content of accordion
-                touchableComponent={TouchableWithoutFeedback}
-                //which type of touchable component you want
-                //It can be the following Touchables
-                //TouchableHighlight, TouchableNativeFeedback
-                //TouchableOpacity , TouchableWithoutFeedback
-                //expandMultiple={multipleSelect}
-                //Do you want to expand mutiple at a time or single at a time
-                renderHeader={renderHeader}
-                //Header Component(View) to render
-                renderContent={renderContent}
-                //Content Component(View) to render
-                duration={400}
-                //Duration for Collapse and expand
-                onChange={setSections}
-                //setting the state of active sections
-                />
-                */
 const styles = StyleSheet.create({
     card__title: { 
         fontSize: 16,
--- views/ReclamosView.js
+++ views/ReclamosView.js
@@ -4,25 +4,16 @@ import { StyleSheet, View, ScrollView } from 'react-native';
 
 import { Text } from 'react-native-paper';
 import ListReclamos from './ListReclamos';
-import Icon from 'react-native-vector-icons/FontAwesome5';
 
 const ReclamosView = ( {navigation} ) => {
     return (
     <View>
       <View style={ styles.view_title }>
         <Text style={ styles.view_text }>Reclamos</Text>
-        <Icon 
-          style={ styles.view_icon } 
-          name='sync' 
-          size={20} 
-          color={ theme.colors.disabled } 
-        />
       </View>
-        <View>
-        <ScrollView>
-          <ListReclamos/>
-        </ScrollView >
-      </View>
+      <ScrollView>
+        <ListReclamos/>
+      </ScrollView >
     </View>
     );
   }
@@ -42,11 +33,6 @@ const styles = StyleSheet.create({
     color: theme.colors.disabled,
     fontSize: 19,
     fontFamily: 'Roboto',
-  },
-  view_icon: {
-    display:'flex',
-    textAlign: 'center',
-    alignItems: 'center'
   }
 });
 
